624e2a843be291c7c142fe8ce6c096f58cd2f6cf
edney <edneysilva20@hotmail.com>
2020-09-08 22:44:37 -0300

    Inserindo o primeiro conteúdo da página

--- Introdução_a_programação_JavaScript/desafio1.js
+++ Introdução_a_programação_JavaScript/desafio1.js
@@ -1,5 +1,5 @@
 // TESTE 01
-let line = gets('11', '7').split(" ");
+let line = gets().split(" ");
 let A = parseInt(line[0]);
 let B = parseInt(line[1]);
 let total = A + B; // Altere o valor da variável com o cálculo esperado
