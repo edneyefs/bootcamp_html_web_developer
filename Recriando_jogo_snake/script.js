54627611324fd620e9a780c967ee22e0a3de675e
edney <edneysilva20@hotmail.com>
2020-09-13 15:19:26 -0300

    Definning directions and make the snake return box

--- Recriando_jogo_snake/script.js
+++ Recriando_jogo_snake/script.js
@@ -15,15 +15,31 @@ function createBG() {
 }
 
 //Criation of snake
-function createSnake(params) {
+function createSnake() {
     for(i = 0; snake.length; i++) {
         context.fillStyle = "green";
         context.fillRect(snake[i].x, box, box);
     }
 }
 
+//Create a block of movement reverse
+document.addEventListener('keydown', update);
+
+function update(event) {
+    if (event.keyCode == 37 && direction != "right") direction = "left";
+    if (event.keyCode == 38 && direction != "down") direction = "up";
+    if (event.keyCode == 39 && direction != "left") direction = "right";
+    if (event.context == 40 && direction != "up") direction = "down";
+}
+
 //Starting the game
 function startGame() {
+    //Making a snake return to box
+    if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if (snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+    if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if (snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+    
     createBG();
     createSnake();
 
@@ -33,8 +49,8 @@ function startGame() {
 
     if (direction == "right") snakeX =+ box; 
     if (direction == "left") snakeX -= box;
-    if (direction == "up") snakeY += box;
-    if (direction == "down") snakeY -= box;
+    if (direction == "up") snakeY -= box;
+    if (direction == "down") snakeY += box;
 
     //Creating a new head of snake
     snake.pop()
