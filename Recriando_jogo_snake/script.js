6bd773a11ff39a0433ca19084671bccb0705bfa7
edney <edneysilva20@hotmail.com>
2020-09-13 14:34:43 -0300

    Criando a cobrinha

--- Recriando_jogo_snake/script.js
+++ Recriando_jogo_snake/script.js
@@ -1,10 +1,23 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
+let snake = []
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+}
 
-function criarBG() {
+function createBG() {
     context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
+function createSnake(params) {
+    for(i = 0; snake.length; i++) {
+        context.fillStyle = "green";
+        context.fillRect(snake[i].x, box, box);
+    }
+}
+
 criarBG();
+createSnake();
