93c6682b9fdd232a8ddcccc8decbaf559cc41e30
edney <edneysilva20@hotmail.com>
2020-09-14 11:36:13 -0300

    Definition of endgame

--- Recriando_jogo_snake/script.js
+++ Recriando_jogo_snake/script.js
@@ -50,6 +50,14 @@ function startGame() {
     if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if (snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
     
+    //Criation a endgame
+    for (i = 1; i < snake.length; i++) {
+        if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+            clearInterval(game);
+            alert('Game Over :(')
+        }
+    }
+
     createBG();
     createSnake();
     drawFood();
