50ff6a48ca8ee750e5e2ec5cdd639f0e4d86cd11
edney <edneysilva20@hotmail.com>
2020-09-14 11:26:44 -0300

    Criation food

--- Recriando_jogo_snake/script.js
+++ Recriando_jogo_snake/script.js
@@ -7,6 +7,10 @@ snake[0] = {
     y: 8 * box
 }
 let direction = "right"
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 //Criation of background
 function createBG() {
@@ -18,8 +22,14 @@ function createBG() {
 function createSnake() {
     for(i = 0; snake.length; i++) {
         context.fillStyle = "green";
-        context.fillRect(snake[i].x, box, box);
+        context.fillRect(snake[i].x, snake[i].y, box, box);
     }
+}
+
+//Creation of food
+function drawFood() {
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box)
 }
 
 //Create a block of movement reverse
@@ -29,7 +39,7 @@ function update(event) {
     if (event.keyCode == 37 && direction != "right") direction = "left";
     if (event.keyCode == 38 && direction != "down") direction = "up";
     if (event.keyCode == 39 && direction != "left") direction = "right";
-    if (event.context == 40 && direction != "up") direction = "down";
+    if (event.keyCode == 40 && direction != "up") direction = "down";
 }
 
 //Starting the game
@@ -42,6 +52,7 @@ function startGame() {
     
     createBG();
     createSnake();
+    drawFood();
 
     //How the snake make move
     let snakeX = snake[0].x;
@@ -52,11 +63,18 @@ function startGame() {
     if (direction == "up") snakeY -= box;
     if (direction == "down") snakeY += box;
 
+    if (snakeX != food.x || snakeY != food.y) {
+        snake.pop();
+    } else {
+        food.x = Math.floor(Math.random() * 15 + 1) * box;
+        food.y = Math.floor(Math.random() * 15 + 1) * box;
+    }
+
     //Creating a new head of snake
-    snake.pop()
+    //snake.pop();
 
     let newHead = {
-        X: snakeX,
+        x: snakeX,
         y: snakeY
     }
 
