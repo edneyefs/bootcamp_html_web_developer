40e9e76f177c0d51d43efec1325e774b6b29c383
edney <edneysilva20@hotmail.com>
2020-09-27 13:26:40 -0300

    Desafios Bootcamp

---
+++ Introdução_a_programação_JavaScript/desafio2.js
@@ -0,0 +1,14 @@
+/*
+Atribua esta operação à variável PROD, mostrando esta de acordo com a mensagem de saída esperada (exemplo abaixo).   
+
+Entrada
+A entrada contém 2 valores inteiros.
+
+Saída
+Exiba a variável PROD conforme exemplo abaixo, tendo obrigatoriamente um espaço em branco antes e depois da igualdade.
+*/
+
+let valor1 = parseInt(gets(3));
+let valor2 = parseInt(gets(9));
+let total = valor1 * valor2; // Altere o valor da variável com o cálculo esperado 
+console.log("PROD = " + total);
---
+++ Introdução_a_programação_JavaScript/desafio3.js
@@ -0,0 +1,16 @@
+var resultado = calculo(100, 5.50);
+var valor1 = 25;
+
+
+let valor1 = parseInt(gets());
+let valor2 = parseInt(gets());
+let valor3 = parseFloat(gets());
+let salary = parseFloat(1 * 1).toFixed(2); // Digite aqui o calculo do salário
+
+function calculo() {
+    return salary = valor2 * valor3;    
+}
+
+
+console.log("NUMBER = " + valor1);
+console.log("SALARY = U$ " + salary.toFixed(2));
